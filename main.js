e59ca07939fde61818c50147734d7d03133a24b3
cyk2019 <48476326+cyk2019@users.noreply.github.com>
2020-04-12 10:29:36 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
-//常量
+//随机时间
 var randomTime = random(2,3)*1000;
 
-//检测无障碍服务是否已经启用
-if(!auto.waitFor())
-{
-    toast("必须开启无障碍服务");
-    exit();
-}
+//检测是否开启无障碍服务
+toast("必须开启无障碍服务");
+auto.waitFor()
+toast("5秒后脚本启动");
+sleep(5000);
 
 //提示信息
-toast("当前屏幕尺寸，高："+device.height+" 宽："+device.width);
+toast("实况足球手游脚本V1.0  启动...");
 sleep(randomTime);
-toast("实况足球手游辅助V1.0 启动中...");
+toast("当前屏幕尺寸 高："+device.height+" 宽："+device.width);
 sleep(randomTime);
 
 //自动设置适合的屏幕宽高
-setScreenMetrics(1080, 2340);
+setScreenMetrics(1080,2340);
 
 //点击比赛栏目
 click(475,165);
@@ -25,11 +24,11 @@ sleep(randomTime);
 click(1630,490);
 sleep(randomTime);
 
-//点击活动1
+//选择活动1
 click(900,350);
 sleep(randomTime);
 
-//点击活动1继续
+//点击活动1的继续
 click(1810,980);
 sleep(randomTime);
 
@@ -37,39 +36,72 @@ sleep(randomTime);
 click(1600,580);
 sleep(randomTime);
 
-var count = 0;//局数计数器
+//局数计数器
+var count = 0;
 
-for(var i = 0;i<50;i++)
+for(var i = 0;i<999;i++)
 {
     startGame();
     count = count + 1;
-    toast("已经踢了"+count+"局");
+    toast("已经踢了 "+count+" 局");
 }
 
+
+/**
+ * 游戏循环部分
+ */
 function startGame()
-{
-    //点击前往比赛
+ {
+     //点击前往比赛
     click(2015,985);
-    sleep(7000);
-    
+    sleep(8000); //队伍匹配时间
+
     //点击下一步
     click(2015,985);
     sleep(randomTime);
-    
-    //点击确认进入比赛
+
+    //进入正式比赛页面
     click(2015,985);
     sleep(10000);
-    
-    for(var i = 1;i<110;i++)
+
+    for(var i = 0;i<110;i++)
     {
         click(2015,985);
         sleep(5000);
-        toast("当前已经点击"+i+"次");
+        toast("已经点击了"+i+"次");
     }
 
     for(var j = 0;j<5;j++)
     {
         click(1200,850);
         sleep(randomTime);   
+    }
+ }
+
+
+ /**
+* 悬浮窗计时器
+*/
+function timeWindow()
+{
+    var w = floaty.rawWindow(
+        <frame gravity="center">
+            <text id="text" textSize="18sp" textColor="#f44336"></text>
+        </frame>
+        );
+    
+    //设置悬浮窗位置
+    w.setPosition(device.height/2.5, device.width/6);
+    //设置悬浮窗尺寸
+    w.setSize(700,200);
+    w.setTouchable(false);
+    
+    setInterval(()=>{w.text.setText(showTime())},1000);
+    
+    function showTime()
+    {
+        var date = new Date();
+        var time = util.format("当前时间 %d:%d:%d", date.getHours(),date.getMinutes(),date.getSeconds());
+        return time;
     }
 }
