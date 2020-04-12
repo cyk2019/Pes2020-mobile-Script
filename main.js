cf67d6d0ac5d213bf0dbd64f7e6fc992251f002b
cyk2019 <48476326+cyk2019@users.noreply.github.com>
2020-04-12 17:12:54 +0800

    V1.1
    解决找图问题

--- main.js
+++ main.js
@@ -7,14 +7,31 @@ auto.waitFor()
 toast("5秒后脚本启动");
 sleep(5000);
 
+//自动适配点击的屏幕宽高
+setScreenMetrics(1080, 2340);
+
 //提示信息
 toast("实况足球手游脚本V1.0  启动...");
 sleep(randomTime);
 toast("当前屏幕尺寸 高："+device.height+" 宽："+device.width);
 sleep(randomTime);
 
-//自动设置适合的屏幕宽高
-setScreenMetrics(1080,2340);
+//检查是否有CaptureScreen文件夹
+toast("自动创建ScreenCapture");
+files.ensureDir("/sdcard/ScreenCapture/");
+sleep(randomTime);
+
+//获取截图权限
+if(!images.requestScreenCapture())
+{
+    toast("获取截图权限失败！");
+    exit();
+}
+else
+{
+    toast("获取截图权限成功！");
+    sleep(randomTime);
+}
 
 //点击比赛栏目
 click(475,165);
@@ -36,14 +53,13 @@ sleep(randomTime);
 click(1600,580);
 sleep(randomTime);
 
-//局数计数器
 var count = 0;
 
-for(var i = 0;i<999;i++)
+for(var i = 0;i<9999;i++)
 {
     startGame();
     count = count + 1;
-    toast("已经踢了 "+count+" 局");
+    toast("已经踢了"+count+"场");
 }
 
 
@@ -51,8 +67,13 @@ for(var i = 0;i<999;i++)
  * 游戏循环部分
  */
 function startGame()
- {
-     //点击前往比赛
+{
+    var status_img; //状态截图
+    var big_img; //截图大图
+    var small_img; //目标小图片
+    var p; //目标坐标
+
+    //点击前往比赛
     click(2015,985);
     sleep(8000); //队伍匹配时间
 
@@ -62,46 +83,63 @@ function startGame()
 
     //进入正式比赛页面
     click(2015,985);
-    sleep(10000);
 
-    for(var i = 0;i<110;i++)
+    //上半场
+    sleep(240000);
+    do{
+        status_img = images.captureScreen();
+        images.save(status_img, "/sdcard/ScreenCapture/index_img.png");
+        sleep(randomTime);
+        big_img = images.read("/sdcard/ScreenCapture/index_img.png");
+        small_img = images.read("/sdcard/TargetPicture/target_1.jpg");
+        p = images.findImage(big_img, small_img);
+        sleep(2000);
+    }while(p==null);
+    big_img.recycle();
+    small_img.recycle();
+    click(2015,985);
+    sleep(randomTime);
+    click(2015,985);
+
+    //下半场
+    sleep(240000);
+    do{
+        status_img = images.captureScreen();
+        images.save(status_img, "/sdcard/ScreenCapture/index_img.png");
+        sleep(randomTime);
+        big_img = images.read("/sdcard/ScreenCapture/index_img.png");
+        small_img = images.read("/sdcard/TargetPicture/target_2.jpg");
+        p = images.findImage(big_img, small_img);
+        sleep(2000);
+    }while(p==null);
+    big_img.recycle();
+    small_img.recycle();
+    click(2015,985);
+    sleep(randomTime);
+    click(2015,985);
+    sleep(5000);
+    click(2015,985);
+    sleep(randomTime);
+    click(2015,985);
+    sleep(5000);
+
+    //出现活动奖励
+    status_img = images.captureScreen();
+    images.save(status_img, "/sdcard/ScreenCapture/index_img.png");
+    sleep(randomTime);
+    big_img = images.read("/sdcard/ScreenCapture/index_img.png");
+    small_img = images.read("/sdcard/TargetPicture/target_3.jpg");
+    p = images.findImage(big_img, small_img);
+    if(!p) //出现活动奖励框
     {
-        click(2015,985);
-        sleep(5000);
-        toast("已经点击了"+i+"次");
+        click(1180,885);
+        sleep(randomTime);
+        click(1180,885);
+        sleep(randomTime);
+        click(1180,885);
+        sleep(randomTime);
     }
-
-    for(var j = 0;j<5;j++)
-    {
-        click(1200,850);
-        sleep(randomTime);   
-    }
- }
-
-
- /**
-* 悬浮窗计时器
-*/
-function timeWindow()
-{
-    var w = floaty.rawWindow(
-        <frame gravity="center">
-            <text id="text" textSize="18sp" textColor="#f44336"></text>
-        </frame>
-        );
-    
-    //设置悬浮窗位置
-    w.setPosition(device.height/2.5, device.width/6);
-    //设置悬浮窗尺寸
-    w.setSize(700,200);
-    w.setTouchable(false);
-    
-    setInterval(()=>{w.text.setText(showTime())},1000);
-    
-    function showTime()
-    {
-        var date = new Date();
-        var time = util.format("当前时间 %d:%d:%d", date.getHours(),date.getMinutes(),date.getSeconds());
-        return time;
-    }
+    big_img.recycle();
+    small_img.recycle();
+    sleep(randomTime);
 }
